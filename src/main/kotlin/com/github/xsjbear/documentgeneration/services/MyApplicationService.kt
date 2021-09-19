26e954ed90046d2f6e5853884bd591c79d9d709c
GitHub Action <action@github.com>
2021-09-19 03:00:08 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/xsjbear/documentgeneration/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.xsjbear.documentgeneration.services
+
+import com.github.xsjbear.documentgeneration.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/xsjbear/documentgeneration/services/MyProjectService.kt
+++ src/main/kotlin/com/github/xsjbear/documentgeneration/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.xsjbear.documentgeneration.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xsjbear.documentgeneration.MyBundle
 
 class MyProjectService(project: Project) {
 
